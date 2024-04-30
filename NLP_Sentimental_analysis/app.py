5f1f5f48b7a717487ae2d7ce36796dc2a599c1a8
D Rohit Kumar <100421996+rkroxk22@users.noreply.github.com>
2024-04-30 15:58:58 +0530

    Update app.py

--- NLP_Sentimental_analysis/app.py
+++ NLP_Sentimental_analysis/app.py
@@ -3,36 +3,6 @@ import pickle
 import requests
 from io import BytesIO
 
-# Custom CSS for dark theme
-custom_css = """
-<style>
-body {
-    background-color: #121212; /* Dark background color */
-    color: #FFFFFF; /* Text color */
-}
-
-.stTextInput>div>div>input {
-    background-color: #333333; /* Dark input field color */
-    color: #FFFFFF; /* Text color for input field */
-}
-
-.stButton>button {
-    background-color: #4CAF50; /* Green color for buttons */
-    color: #FFFFFF; /* Text color for buttons */
-    border-color: #4CAF50; /* Border color for buttons */
-}
-
-.stButton>button:hover {
-    background-color: #45a049; /* Darker green color for buttons on hover */
-}
-
-.stButton>button:active {
-    background-color: #3e8e41; /* Darker green color for buttons when clicked */
-}
-</style>
-"""
-st.markdown(custom_css, unsafe_allow_html=True)
-
 # Download the pickle file
 url = 'https://github.com/rkroxk22/Streamlit_Apps/raw/main/NLP_Sentimental_analysis/IMDB_sentiment_analysis.pkl'
 response = requests.get(url)
