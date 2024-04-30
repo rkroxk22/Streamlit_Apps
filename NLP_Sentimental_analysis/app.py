1b8de73ddd434a31a764449b79c01e7f2b9d28c2
D Rohit Kumar <100421996+rkroxk22@users.noreply.github.com>
2024-04-30 15:56:59 +0530

    Update app.py

--- NLP_Sentimental_analysis/app.py
+++ NLP_Sentimental_analysis/app.py
@@ -1,15 +1,8 @@
 import streamlit as st
 import pickle
-import requests
-from io import BytesIO
-
-# Download the pickle file
-url = 'https://github.com/rkroxk22/Streamlit_Apps/raw/main/NLP_Sentimental_analysis/IMDB_sentiment_analysis.pkl'
-response = requests.get(url)
-pickle_bytes = BytesIO(response.content)
 
 # Load model
-model = pickle.load(pickle_bytes)
+model = pickle.load(open('IMDB_sentiment_analysis.pkl', 'rb'))
 
 # Create title
 st.title('Sentiment Analysis Model')
@@ -32,3 +25,4 @@ if submit and review.strip() != '':
 # Check if the submit button is clicked but the review text input is empty
 elif submit and review.strip() == '':
     st.warning('Please enter a review before predicting.')
+
